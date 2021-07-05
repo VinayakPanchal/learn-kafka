19c9759d2e887b2ba8f84c16097ae189259819cf
vinayakpanchal <vinayakpanchal007@outlook.com>
2021-07-04 23:23:54 -0400

    added logging

--- src/main/java/com/vinayak/java/kafka/controller/HelloController.java
+++ src/main/java/com/vinayak/java/kafka/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.vinayak.java.kafka.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String homeEndpoint(){
+        LOG.info("Reached Hello Controller");
         return "Welcome to Vinayak's Ecosystem";
     }
 }
